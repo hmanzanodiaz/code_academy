f03270c80f2359d9fa73edee566ff23c2a8b4676
hector the protector <hmanzanodiaz@gmail.com>
2020-07-15 14:30:12 -0700

    master repo

---
+++ documentation.py
@@ -0,0 +1,7 @@
+# command line
+# https://www.codecademy.com/learn/paths/computer-science/tracks/cspath-development-skills/modules/learn-the-command-line-navigation/cheatsheet
+# https://www.codecademy.com/articles/command-line-commands
+# https://www.ostechnix.com/quickly-create-files-inside-nested-directories-linux/
+
+# GIT
+# https://www.codecademy.com/paths/computer-science/tracks/cspath-development-skills/modules/learn-git-git-workflow-u/articles/getting-started-git-and-github
